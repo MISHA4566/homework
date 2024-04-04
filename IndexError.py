1ef64e064dd37767ce8522a09cc2fcaef7859762
MISHA4566 <kagantsovmisha55@gmail.com>
2024-04-04 22:33:25 +0300

    commit 3

--- IndexError.py
+++ IndexError.py
@@ -1,17 +1,7 @@
-def write_to_file(user_input, filename):
-    try:
-        # Відкриття файлу для запису
-        with open(filename, 'w') as file:
-            # Запис рядка у файл
-            file.write(user_input)
-        print(f"Рядок '{user_input}' успішно записано у файл '{filename}'.")
-    except IndexError:
-        print("Помилка: неправильний індекс у списку.")
-    except Exception as e:
-        print(f"Помилка при записі у файл: {e}")
-
-# Отримання рядка від користувача
-user_text = input("Введіть рядок для запису у файл: ")
-
-# Виклик функції для запису у файл
-write_to_file(user_text, 'feli.txt')
+try:
+    my_list = [1, 2, 3]
+    index = 5
+    value = my_list[index]
+    print("Значення за індексом", index, ":", value)
+except IndexError:
+    print("Помилка: неправильний індекс")
